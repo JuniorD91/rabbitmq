3b46c486903bd93fa3612401084808fa2f605066
Francisco Junior <fjuniordourado@gmail.com>
2020-03-03 11:01:36 -0300

    refatorando package

--- rabbitmq-ampq-hello-world/src/main/java/com/rabbitmq/Application.java
+++ rabbitmq-ampq-hello-world/src/main/java/com/rabbitmq/Application.java
@@ -1,13 +1,13 @@
-package com.rabbitmq.rabbitmqampqhelloworld;
+package com.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqAmpqHelloWorldApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitmqAmpqHelloWorldApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
