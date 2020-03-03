d1780efc3541113310f298a44fe6b6b649cf184b
Francisco Junior <fjuniordourado@gmail.com>
2020-03-02 22:28:24 -0300

    criando primeiro projeto do rabbitmq hello world

---
+++ rabbitmq-ampq-hello-world/src/main/java/com/rabbitmq/rabbitmqampqhelloworld/RabbitmqAmpqHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rabbitmqampqhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAmpqHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqAmpqHelloWorldApplication.class, args);
+    }
+
+}
---
+++ rabbitmq-ampq-hello-world/src/test/java/com/rabbitmq/rabbitmqampqhelloworld/RabbitmqAmpqHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rabbitmqampqhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAmpqHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
