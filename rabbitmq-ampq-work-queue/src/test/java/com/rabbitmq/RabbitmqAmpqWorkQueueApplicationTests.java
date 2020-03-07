74952a7b1a26df55dffaa758c2455e4d637c137d
Francisco Junior <fjuniordourado@gmail.com>
2020-03-07 17:39:18 -0300

    criando projeto do tutorial 2 - rabbitmq usando conceito de work queue

---
+++ rabbitmq-ampq-work-queue/src/test/java/com/rabbitmq/RabbitmqAmpqWorkQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAmpqWorkQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
